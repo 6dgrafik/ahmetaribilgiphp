f026ab5446f0236f2d28dd0f76d7d59864958ce6
6dgrafik <6dgrafik@gmail.com>
2022-03-21 19:36:09 +0300

    Dosyalar oluşturuldu

---
+++ admin/baglan.php
@@ -0,0 +1,3 @@
+<?php
+$db = new PDO('mysql:host=localhost;dbname=aribilgi;charset=UTF8','root','');
+?>
---
+++ admin/logout.php
@@ -0,0 +1,13 @@
+
+<?php
+
+session_start();
+
+session_destroy();
+
+if(!isset($_SESSION['kadi'])){
+    die('Giriş Yetkiniz Yoktur');
+}
+
+?>
+

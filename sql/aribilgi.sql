46fed9b65d35174b639588f85a7519289838b84a
6dgrafik <6dgrafik@gmail.com>
2022-03-21 19:52:34 +0300

    sql güncellendi

---
+++ sql/aribilgi.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 21 Mar 2022, 17:51:50
+-- Sunucu sürümü: 10.4.21-MariaDB
+-- PHP Sürümü: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `aribilgi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kategoriler`
+--
+
+CREATE TABLE `kategoriler` (
+  `id` int(11) NOT NULL,
+  `kategori` varchar(25) NOT NULL,
+  `katturu` varchar(15) NOT NULL,
+  `ustkat` varchar(30) NOT NULL,
+  `meta` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `kategoriler`
+--
+
+INSERT INTO `kategoriler` (`id`, `kategori`, `katturu`, `ustkat`, `meta`) VALUES
+(2, 'Web Tasarım', 'Ana Kategori', '-', 'Web Tasarım ve kodlama eğitimi'),
+(3, 'Grafik Tasarımı', 'Ana Kategori', '-', 'Grafik Tasarımı üzerine eğitimler.'),
+(4, 'Dijital Pazarlama', 'Ana Kategori', '-', 'Dijital pazarlama ile ilgili eğitimler'),
+(5, 'Html', 'Alt Kategori', 'Web Tasarım', 'Html Hakkında gerekli eğitimler');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `kategoriler`
+--
+ALTER TABLE `kategoriler`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kategoriler`
+--
+ALTER TABLE `kategoriler`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

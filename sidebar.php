18b8a07870fa9eacdbbba653a7dcf564d64f7807
6dgrafik <6dgrafik@gmail.com>
2022-04-11 21:38:11 +0300

    11 Nisan ders sonu

---
+++ sidebar.php
@@ -0,0 +1,25 @@
+<?php
+require_once('header.php');?>
+<!-- Side bar section strat -->
+
+<div class="col-md-3">
+    <div class="row">
+        <div class="col-12">
+            <div class="card shadow">
+                <div class="card-body">
+                <form method="get">
+                    <div class="form-group">
+                        <input type="search" name="arama" placeholder="Blog Ara" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Arama Yap" class="btn btn-mor w-100">
+                    </div>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Side bar section strat -->
+<?php require_once('footer.php'); ?>
